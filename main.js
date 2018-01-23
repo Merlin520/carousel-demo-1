cf32e902178297e6afb361572f92a25ecd277894
meiya <meiya90hou@163.com>
2018-01-23 21:13:58 +0800

    简单轮播

---
+++ main.js
@@ -0,0 +1,17 @@
+$(p1).on('click',function () {
+    $(image).css({
+        transform:'translateX(0px)'
+    })
+});
+
+$(p2).on('click',function () {
+    $(image).css({
+        transform:'translateX(-400px)'
+    })
+});
+
+$(p3).on('click',function () {
+    $(image).css({
+        transform:'translateX(-800px)'
+    })
+});
