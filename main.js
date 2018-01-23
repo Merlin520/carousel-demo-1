3ab212435643628b321622e29e5284bcea84a1a9
meiya <meiya90hou@163.com>
2018-01-23 22:43:10 +0800

    轮播优化

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
-$(p1).on('click',function () {
-    $(image).css({
-        transform:'translateX(0px)'
+// $(p1).on('click',function () {
+//     $(image).css({
+//         transform:'translateX(0px)'
+//     })
+// });
+// $(p2).on('click',function () {
+//     $(image).css({
+//         transform:'translateX(-400px)'
+//     })
+// });
+// $(p3).on('click',function () {
+//     $(image).css({
+//         transform:'translateX(-800px)'
+//     })
+// });
+
+
+var allButtons = $('#buttons > span');
+
+
+for(let i=0;i<allButtons.length;i++){
+    $(allButtons[i]).on('click',function (x) {
+
+        var index = $(x.currentTarget).index();
+        var n = index * -300;
+
+        $('#image').css({
+            transform:'translate(' + n + 'px)'
+        })
     })
-});
+}
 
 
-$(p2).on('click',function () {
-    $(image).css({
-        transform:'translateX(-400px)'
-    })
-});
 
 
-$(p3).on('click',function () {
-    $(image).css({
-        transform:'translateX(-800px)'
-    })
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
